3ce8bda429c174f094628738a8805c665b0781d9
Marek Suscak <marek.suscak@gmail.com>
2019-02-06 18:43:10 +0100

    Fix a typo and improve the comments

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -18,13 +18,16 @@
 // zamyla:50cI2vYkF0YU2 => LOL
 
 /**
- * Analysis:
+ * Here's how the algorithm progresses in 5 steps. 
+ * X is a letter placeholder, \0 is a null character.
+ * 5 lines = 5 loops
  *
- * X\0____
- * XX\0___
- * XXX\0__
- * XXXX\0_
- * XXXXX\0
+ *    1 2 3 4 5 6
+ * 1) X\0\0\0\0\0
+ * 2) X X\0\0\0\0
+ * 3) X X X\0\0\0
+ * 4) X X X X\0\0
+ * 5) X X X X X\0
  */
 
 int main(int argc, string argv[])
@@ -36,7 +39,9 @@ int main(int argc, string argv[])
     }
 
     // TODO: how can i compute the length of the string below?
-    const int letters_count = 57;
+
+    // 26 lowercase + 26 uppercase + \0 counts as 1 letter = 53
+    const int letters_count = 53;
 
     // TODO: possible improvement => https://en.wikipedia.org/wiki/Letter_frequency
     string letters = "\0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -48,6 +53,8 @@ int main(int argc, string argv[])
 
     char key_candidate[6] = "\0\0\0\0\0\0";
 
+    // The outermost loop advances least frequently compared to its descendants,
+    // therefore according to the scheme above, it maps to the last (fifth) letter.
     for (int fifth = 0; fifth < letters_count; fifth++)
     {
         for (int fourth = 0; fourth < letters_count; fourth++)
@@ -56,14 +63,23 @@ int main(int argc, string argv[])
             {
                 for (int second = 0; second < letters_count; second++)
                 {
+		    // The innermost loop advances most frequently compared to its ascendants,
+		    // therefore according to the scheme above, it maps to the first letter.
+		    // We could initialize it to 0 but that would be an empty string
+		    // (i.e. no password) which is not allowed (should not be!)
                     for (int first = 1; first < letters_count; first++)
                     {
-                        key_candidate[0] = letters[first];
-                        key_candidate[1] = letters[second];
-                        key_candidate[2] = letters[third];
-                        key_candidate[3] = letters[fourth];
-                        key_candidate[4] = letters[fifth];
+                        key_candidate[0] = letters[first];  // 1)
+                        key_candidate[1] = letters[second]; // 2)
+                        key_candidate[2] = letters[third];  // 3)
+                        key_candidate[3] = letters[fourth]; // 4)
+                        key_candidate[4] = letters[fifth];  // 5)
 
+			// It's important to realize that most string manipulation functions in C
+			// stop looping over the characters once they encounter a null character.
+			// That's essentially what this algorithm leverages when it initializes
+			// the key_candidate full of null characters and progresses from 1-letter
+			// all the way through 5-letter strings.
                         if (strcmp(crypt(key_candidate, salt), hash) == 0)
                         {
                             printf("%s\n", key_candidate);
